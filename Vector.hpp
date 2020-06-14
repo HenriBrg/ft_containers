4feb878640946a031373f403367d008e43a2e5c1
HenriBrg <hnr.berger@gmail.com>
2020-06-14 22:36:28 +0200

    Update

--- Vector.hpp
+++ Vector.hpp
@@ -1,7 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: henri <henri@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/14 00:38:29 by henri             #+#    #+#             */
+/*   Updated: 2020/06/14 00:50:00 by henri            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 # ifndef VECTOR_HPP
 # define VECTOR_HPP
 
 namespace ft {
+
+/* T doit satisfaire aux exigences de CopyAssignable et CopyConstructible. */
+
+template <class T, class Allocator = std::allocator<T> >
+
+class vector {
+
+};
 
 };
 
