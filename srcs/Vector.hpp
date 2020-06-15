6fefe2d1c8f65b1798f3d8ab1f1351e7d9d86349
HenriBrg <hnr.berger@gmail.com>
2020-06-15 22:14:53 +0200

    Update

--- srcs/Vector.hpp
+++ srcs/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: henri <henri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/14 00:38:29 by henri             #+#    #+#             */
-/*   Updated: 2020/06/14 00:50:00 by henri            ###   ########.fr       */
+/*   Updated: 2020/06/15 21:56:14 by henri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 
 namespace ft {
 
-/* T doit satisfaire aux exigences de CopyAssignable et CopyConstructible. */
-
 template <class T, class Allocator = std::allocator<T> >
 
-class vector {
+class Vector {
+
+	public:
+
 
 };
 
